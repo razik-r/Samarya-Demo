bbf9741f28cfd4ce9055625ce21c0f8145b44853
razik-r <razikrehim123@gmail.com>
2026-05-27 14:13:25 +0530

    404 redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import ScrollToTop from './ScrollToTop.jsx';
-import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-  <ScrollToTop />
-  <StrictMode>
-    
-    <App />
-  
-  </StrictMode>
-  </HashRouter>
+  <BrowserRouter basename="/Samarya-Demo/">
+    <ScrollToTop />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 )
