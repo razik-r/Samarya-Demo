6f612bc22eca87c3f825fce87941b52237c5d0d5
razik-r <razikrehim123@gmail.com>
2026-05-27 13:27:12 +0530

    link name change and using hash router

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import ScrollToTop from './ScrollToTop.jsx';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <ScrollToTop />
   <StrictMode>
     
     <App />
   
   </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
